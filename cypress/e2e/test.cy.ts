d1c77f13525f2de4e04c2718f85835aa8ee37110
AyobomiOmojola <ayomojola12@gmail.com>
2024-08-19 20:21:53 +0100

    made some changes

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -14,19 +14,19 @@ describe('template spec', () => {
     cy.visit(constantData.url)
     // check presence of logo
     homePage.getLogo()
-    // check presence of searchbar
-    // homePage.getSearchBar()
-    // //check number of navbar items
-    // homePage.getNumberOfMenuItems()
-    // //check sections visibility, number and text assertion
-    // homePage.getFirstSection()
-    // homePage.getSecondSection()
-    // homePage.getThirdSection()
-    // //check social media links are live
-    // homePage.getSocialMediaLinks()
-    // //check if search result is present
-    // homePage.getSearchResults()
-    // //check clear icon is functional
+    //check presence of searchbar
+    homePage.getSearchBar()
+    //check number of navbar items
+    homePage.getNumberOfMenuItems()
+    //check sections visibility, number and text assertion
+    homePage.getFirstSection()
+    homePage.getSecondSection()
+    homePage.getThirdSection()
+    //check social media links are live
+    homePage.getSocialMediaLinks()
+    //check if search result is present
+    homePage.getSearchResults()
+    //check clear icon is functional
     homePage.typeAndClearSearchBar()
 
   })
